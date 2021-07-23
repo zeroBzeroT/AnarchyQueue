7fba071990cb706c7070bc0f3aeb7be418a84ce7
nothub <48992448+nothub@users.noreply.github.com>
2021-07-23 16:58:27 +0200

    correct logger

--- src/main/java/org/zeroBzeroT/anarchyqueue/Queue.java
+++ src/main/java/org/zeroBzeroT/anarchyqueue/Queue.java
@@ -8,7 +8,6 @@ import com.velocitypowered.api.event.proxy.ListenerBoundEvent;
 import com.velocitypowered.api.event.proxy.ListenerCloseEvent;
 import com.velocitypowered.api.proxy.ProxyServer;
 import com.velocitypowered.api.proxy.server.RegisteredServer;
-import jdk.jpackage.internal.Log;
 
 import java.util.Optional;
 
@@ -22,17 +21,17 @@ public class Queue {
 
     @Subscribe
     public void onListenerBound(ListenerBoundEvent e) {
-        Log.info("ListenerBoundEvent: " + e.getAddress().toString() + " " + e.getListenerType().toString());
+        Main.getInstance().log.info("ListenerBoundEvent: " + e.getAddress().toString() + " " + e.getListenerType().toString());
     }
 
     @Subscribe
     public void onListenerClose(ListenerCloseEvent e) {
-        Log.info("ListenerCloseEvent: " + e.getAddress().toString() + " " + e.getListenerType().toString());
+        Main.getInstance().log.info("ListenerCloseEvent: " + e.getAddress().toString() + " " + e.getListenerType().toString());
     }
 
     @Subscribe
     public void onKickedFromServer(KickedFromServerEvent e) {
-        Log.info("KickedFromServerEvent: " + e.getPlayer().getUsername() + " " + e.getServer().getServerInfo().getName() + " " + e.getServer().getServerInfo().getAddress().toString());
+        Main.getInstance().log.info("KickedFromServerEvent: " + e.getPlayer().getUsername() + " " + e.getServer().getServerInfo().getName() + " " + e.getServer().getServerInfo().getAddress().toString());
     }
 
     @Subscribe
@@ -40,15 +39,15 @@ public class Queue {
         Optional<RegisteredServer> serverOpt = e.getInitialServer();
         if (serverOpt.isPresent()) {
             RegisteredServer server = serverOpt.get();
-            Log.info("PlayerChooseInitialServerEvent: " + e.getPlayer().getUsername() + " " + server.getServerInfo().getName() + " " + server.getServerInfo().getAddress().toString());
+            Main.getInstance().log.info("PlayerChooseInitialServerEvent: " + e.getPlayer().getUsername() + " " + server.getServerInfo().getName() + " " + server.getServerInfo().getAddress().toString());
         } else {
-            Log.info("PlayerChooseInitialServerEvent: " + e.getPlayer().getUsername() + " no initial server");
+            Main.getInstance().log.info("PlayerChooseInitialServerEvent: " + e.getPlayer().getUsername() + " no initial server");
         }
     }
 
     @Subscribe
     public void onServerConnected(ServerConnectedEvent e) {
-        Log.info("ServerConnectedEvent: " + e.getPlayer().getUsername() + " " + e.getServer().getServerInfo().getName() + " " + e.getServer().getServerInfo().getAddress().toString());
+        Main.getInstance().log.info("ServerConnectedEvent: " + e.getPlayer().getUsername() + " " + e.getServer().getServerInfo().getName() + " " + e.getServer().getServerInfo().getAddress().toString());
     }
 
     public void flushQueue() {
